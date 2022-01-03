1b54c571a93f1bebeaca86080b78b7f384adb2d3
maiyama <myam0118@gmail.com>
2022-01-03 09:38:50 +0900

    do not use Swinject for Routing

--- maiyama/Router.xctemplate/___FILEBASENAME___Router.swift
+++ maiyama/Router.xctemplate/___FILEBASENAME___Router.swift
@@ -12,13 +12,13 @@ protocol ___VARIABLE_productName:identifier___Routing {
 extension ___VARIABLE_productName:identifier___Routing {
     @MainActor
     func push___VARIABLE_productName:identifier___(from originVC: UIViewController) {
-        let destinationVC = resolver.resolve(___VARIABLE_productName:identifier___ViewController.self)!
+        let destinationVC = ___VARIABLE_productName:identifier___ViewController()
         originVC.navigationController?.pushViewController(destinationVC, animated: true)
     }
     
     @MainActor
     func present___VARIABLE_productName:identifier___(from originVC: UIViewController) {
-        let destinationVC = resolver.resolve(___VARIABLE_productName:identifier___ViewController.self)!
+        let destinationVC = ___VARIABLE_productName:identifier___ViewController()
         let navigationVC = UINavigationController(
             rootViewController: destinationVC
         )
